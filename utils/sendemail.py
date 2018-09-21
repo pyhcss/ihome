bdc1dbe718e878bf602d46d54dd459b323e7dd21
yunlong <909576924@qq.com>
2018-09-22 01:52:28 +0800

    修复邮箱验证码模块

--- utils/sendemail.py
+++ utils/sendemail.py
@@ -2,19 +2,22 @@
 
 import smtplib
 from email.mime.text import MIMEText
-from email.header import Header
+# from email.header import Header
 
 
 def sendemail(address,code,times="5"):
-    from_mail = "admin@newzn.cn"
+    smtp_address = "smtp.163.com"
+    from_mail = "newzn_ihome@163.com"
+    from_pwd = "******"
     to_mail = address
 
-    message = MIMEText("您好,欢迎注册ihom网站,您的验证码是"+ code +",请于"+ times +"分钟之内注册,本邮箱非真实邮箱,请勿回复,谢谢使用","plain","utf-8")
-    message["From"] = Header("","utf-8")
-    message["To"] = Header("","utf-8")
-    message["Subject"] = Header("ihome","utf-8")
+    message = MIMEText("您好,欢迎注册ihom网站,您的验证码是"+ code +",请于"+ times +"分钟之内注册,谢谢使用","plain","utf-8")
+    message["From"] = from_mail
+    message["To"] = to_mail
+    message["Subject"] = "ihome"
     try:
-        smtp_server = smtplib.SMTP("localhost")
+        smtp_server = smtplib.SMTP(smtp_address,port=25)
+        smtp_server.login(from_mail,from_pwd)
         smtp_server.sendmail(from_mail,to_mail,message.as_string())
         return "发送成功"
     except Exception as e:
